b559615cae00800df6ac4c5e28a4b7a02e705319
alessiovietri <info@alessiovietri.it>
2024-01-09 09:36:39 +0100

    Completato template

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
-// Test funzionamento
-console.log('Template HTML CSS JS');
+// Estraggo la funzione createApp dall'oggetto Vue
+const { createApp } = Vue;
+
+// Creo l'istanza di Vue da mondare in pagina
+createApp({
+    data() {
+        return {
+            message: 'Template HTML CSS Vue',
+        };
+    }
+  // Monto l'istanza di Vue in pagina
+}).mount('#app');
